adc0edcf4704b8d0ff6508d6056e2f42efefd727
Ladis <thedragonladis@gmail.com>
2021-01-14 19:04:23 +0100

    Copy static files to the output folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'none',
   entry: "./src/index.js",
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin( { cleanStaleWebpackAssets: false }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static', to: 'static'}
+      ]
+    }
+    ),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
 };
